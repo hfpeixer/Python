3dd257791b80d6a6d152518a09994063b4f843bb
Hewerson Peixer <hfpeixer@hotmail.com>
2023-02-26 00:57:23 -0300

    Versão Final

--- Pacote/Desafios/Mundo2/Desafio71.py
+++ Pacote/Desafios/Mundo2/Desafio71.py
@@ -1,7 +1,29 @@
 print('=' * 15, 'CAIXA ELETRÔNICO', '=' * 15)
 
-
 saque = int(input('Qual o valor do saque? R$ '))
+total = saque
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R$ {ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('Volte sempre')
+
+###     OUTRA OPÇÃO     ####
+'''saque = int(input('Qual o valor do saque? R$ '))
 milhar = saque // 50
 milhar_total = saque // 50 * 50
 sobra = saque - milhar_total
@@ -26,5 +48,5 @@ if dezena != 0:
     print(f'Total de {dezena:.0f} cédula de R$ 10')
 if unidade != 0:
     print(f'Total de {unidade:.0f} cédula de R$ 1')
-
+'''
 
