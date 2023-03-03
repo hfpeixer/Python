d8501829ac1e5f86a15873d518c50bacc3ed4a53
Hewerson Peixer <hfpeixer@hotmail.com>
2023-03-03 00:58:58 -0300

    Versão Final

---
+++ Pacote/Desafios/Mundo2/Desafio74.py
@@ -0,0 +1,7 @@
+from random import randint
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print(f'Os números sorteados foram: ', end='')
+for n in numeros:
+    print(f'{n} ', end='')
+print(f'\nO menor valor sorteado é {min(numeros)}')
+print(f'O maior valor sorteado foi {max(numeros)}')
