e9be89f1f617d719561924b46ab131c1e00a1614
Hewerson Peixer <hfpeixer@hotmail.com>
2023-03-10 11:27:26 -0300

    Versão Final

--- Pacote/Desafios/Mundo2/Desafio72.py
+++
@@ -1,9 +0,0 @@
-t = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatrorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-while True:
-    n = int(input('Digite um número de 0 à 20: '))
-    if n >= 0 and n <= 20:
-        break
-    else:
-        n = int(input('Número inválido!!! Digite um número de 0 à 20: '))  
-        
-print(f'Você digitou o número {t[n]}')
--- Pacote/Desafios/Mundo2/Desafio73.py
+++
@@ -1,9 +0,0 @@
-times = 'Corinthians', 'Palmeiras', 'Santos', 'Flamengo', 'Inter', 'Gremio', 'São Paulo', 'Atlético Mineiro', 'Atlético Paranaense', 'Cruzeiro', 'Botafogo', 'Bahia', 'Fluminense', 'Chapecoense', 'Ceará', 'Vasco', 'Sport', 'América Mineiro', 'Vitória', 'Paraná'
-time2 = 'Chapecoense'
-print(f'Os cincos primeiros times são: {times[:5]}')
-print('=' * 10)
-print(f'Os últimos quatro colocados são: {times[-4:]}')
-print('=' * 10)
-print(f'Em Ordem alfabética: {sorted(times)}')
-print('=' * 10)
-print(f'O time da Chapeconense está na {times.index(time2)+1} posição')
